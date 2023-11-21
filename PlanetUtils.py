53f38e49d74ef3c4794c26e6f91692f0b0d73a24
Charlie Turner <84701489+charlie-turner-314@users.noreply.github.com>
2023-11-21 14:17:54 +1000

    template

---
+++ PlanetUtils.py
@@ -0,0 +1,18 @@
+
+def PlanetSearch(search_path):
+    """
+    Search a given area of interest for Planet imagery
+    :param search_path: path to a json file containing search body
+    :requires: Planet API key be set in environment variable
+    :return: a list of Planet items (json result from API)
+    """
+    pass
+
+def PlanetOrder():
+    """
+    Order a given search result.
+    """
+    pass
+
+def PlanetDownload(orderID:str, downloadPath:str):
+    pass
