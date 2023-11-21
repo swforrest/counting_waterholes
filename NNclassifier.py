13a14dfb059796d235bc98df9c567d81da4c95fd
Charlie Turner <84701489+charlie-turner-314@users.noreply.github.com>
2023-11-21 14:19:00 +1000

    simplify

--- NNclassifier.py
+++ NNclassifier.py
@@ -5,127 +5,101 @@ import os.path as path
 import scipy.cluster
 import scipy.spatial
 import argparse
-from dotenv import load_dotenv
+import yaml
 from datetime import datetime
 
 """
+Intended usage:
+    Automatically called once per day
+    Downloads .tif files into directory
+    Runs the classifier on each .tif file (with preprocessing)
+    Processes the outputs
+    Saves the results to a csv file. 
+
+Optionally?:
+    Save some random images to a folder for manual checking after the fact
+
 Usage: python NNclassifier.py -d <.tif directory> -o <output file name>
 """
-# ENVIRONMENT
-# read paths in from .env
-load_dotenv()
-PYTHON_PATH     = os.getenv("PYTHON_PATH")
-YOLO_PATH       = os.getenv("YOLO_PATH")
-DATA_PATH       = os.getenv("DATA_PATH")
-CLASS_PATH      = os.getenv("CLASSIFICATION_PATH")
-WEIGHTS_PATH    = os.getenv("YOLO_WEIGHTS")
-# Parse command line arguments
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--directory", help="Directory containing .tif files to be classified", required=False)
-    parser.add_argument("-i", "--images", help="Directory of segmented 416x416 images", required=False)
-    parser.add_argument("-t", "--text", help="Directory of text files with classifications", required=False)
-    parser.add_argument("-o", "--output", help="Output file name", required=False, default="BoatsClassified_<DATETIME>.csv")
-    args = parser.parse_args()
-# Set variables
-    IMG_DIRECTORY = args.images
-    TIF_DIRECTORY = args.directory
-    TEXT_DIRECTORY = args.text
 
 
-    OUTFILE = args.output if args.output != "BoatsClassified_<DATETIME>.csv" else f"BoatsClassified_{datetime.now().strftime('%Y%m%d%H%M%S')}"
-
-## CONSTANTS 
-CONF_THRESHOLD                  = 0.5
-STAT_DISTANCE_CUTOFF_PIX        = 6
-MOVING_DISTANCE_CUTOFF_PIX      = 10
-STAT_DISTANCE_CUTOFF_LATLONG    = 0.00025
-MOVING_DISTANCE_CUTOFF_LATLONG  = 0.0003
-
-days = []
 def main():
     """
     Validate input.
     Run the classifier on each image in the directory.
     """
-    # Validate input
-    if not validate_input():
-        exit(1)
-    if IMG_DIRECTORY:
-        classify_images(IMG_DIRECTORY)
-    elif TIF_DIRECTORY:
-        classify_directory(TIF_DIRECTORY)
-    elif TEXT_DIRECTORY:
-        classify_text(TEXT_DIRECTORY)
+    classify_directory(config.get("tif_dir"))
+
+def process_tif(file, data_path, stat_cutoff, moving_cutoff):
+    """
+    Processes a single tif file
+    """
+    classifications, _ = detect(python=config["python"], weights=config["weights"], yolo_dir=config["yolo_dir"], tif_dir = config["tif_dir"] , file_name = file)
+    # split into moving and static boats
+    static = [c for c in classifications if c[3] == 0]
+    moving = [c for c in classifications if c[3] == 1]
+    # cluster each set separately
+    static_clusters = cluster(static, stat_cutoff)
+    moving_clusters = cluster(moving, moving_cutoff)
+    # process each set separately
+    static_boats = process_clusters(static_clusters)
+    moving_boats = process_clusters(moving_clusters)
+    # within the file, need to convert coordinates to lat/long
+    static_boats = pixel2latlong(static_boats, file, config["tif_dir"])
+    moving_boats = pixel2latlong(moving_boats, file, config["tif_dir"])
+    # Clean Up Required Directories
     remove(data_path)
+    remove("tempPNG")
+    # add to the list of all boats
+    return static_boats, moving_boats
 
 def classify_directory(directory):
     """
     Use for directory of tiff images. Preprocesses, classifies, clusters.
     """
-    if TIF_DIRECTORY is None:
-        print("write_to_csv: This function cannot be called from an external script")
-        exit(1)
-    init()
+    days = {ics.get_date_from_filename(file) for file in os.listdir(directory)}
+    days.remove(None)
+    allFiles = os.listdir(directory)
     for i, day in enumerate(days):
         print(f"Classifying day {i+1} of {len(days)} - {day} ({i/len(days)*100:.2f}%)")
-        allFiles = os.listdir(TIF_DIRECTORY)
         files = [file for file in allFiles if ics.get_date_from_filename(file) == day]
-        all_static_boats = []
-        all_moving_boats = []
-        for file in files:
-            classifications, _ = detect(file=file)
-            # split into moving and static boats
-            static = [c for c in classifications if c[3] == 0]
-            moving = [c for c in classifications if c[3] == 1]
-            # cluster each set separately
-            static_clusters = cluster(static, STAT_DISTANCE_CUTOFF_PIX)
-            moving_clusters = cluster(moving, MOVING_DISTANCE_CUTOFF_PIX)
-            # process each set separately
-            static_boats = process_clusters(static_clusters)
-            moving_boats = process_clusters(moving_clusters)
-            # within the file, need to convert coordinates to lat/long
-            static_boats = pixel2latlong(static_boats, file)
-            moving_boats = pixel2latlong(moving_boats, file)
-            # add to the list of all boats
-            all_static_boats.extend(static_boats)
-            all_moving_boats.extend(moving_boats)
-            remove(DATA_PATH)
-            remove("tempPNG")
-            remove("classifier")
-            remove(CLASS_PATH)
+        all_static_boats, all_moving_boats = zip(*[process_tif(file, 
+                                                               config["img_path"], 
+                                                               config["STAT_DISTANCE_CUTOFF_PIX"], 
+                                                               config["MOVING_DISTANCE_CUTOFF_PIX"]) 
+                                                   for file in files])
         # once a day has been classified, need to cluster again
-        static_boats = cluster(all_static_boats, STAT_DISTANCE_CUTOFF_LATLONG)
-        moving_boats = cluster(all_moving_boats, MOVING_DISTANCE_CUTOFF_LATLONG)
+        static_boats = cluster(all_static_boats, config["STAT_DISTANCE_CUTOFF_LATLONG"])
+        moving_boats = cluster(all_moving_boats, config["MOVING_DISTANCE_CUTOFF_LATLONG"])
         # process again
         static_boats = process_clusters(static_boats)
         moving_boats = process_clusters(moving_boats)
         # write to csv
-        write_to_csv(static_boats, day)
-        write_to_csv(moving_boats, day)
+        write_to_csv(static_boats, day, config["OUTFILE"])
+        write_to_csv(moving_boats, day, config["OUTFILE"])
 
-def classify_images(images_dir):
+def classify_images(images_dir, STAT_DISTANCE_CUTOFF_PIX, OUTFILE):
     """
-        Use when images are already split into 416x416 images.
-        Simply runs the classifier and clusters.
+    Use when images are already split into 416x416 images.
+    Simply runs the classifier and clusters.
     """
     dirs = [path.join(images_dir, dir) for dir in os.listdir(images_dir)]
     # read a day of images at a time
     for dir in dirs:
         day = path.basename(dir)
         day = day.split("_").join("/")
-        classifications, _ = detect(dir=dir) 
+        classifications, _ = detect(python=config["python"], weights=config["weights"], yolo_dir=config["yolo_dir"], img_dir = dir) 
         # cluster
         clusters = cluster(classifications, STAT_DISTANCE_CUTOFF_PIX)
         # process
         boats = process_clusters(clusters)
         # write to csv
-        write_to_csv(boats, day)
+        write_to_csv(boats, day, OUTFILE)
 
-def classify_text(dir):
+def classify_text(dir, STAT_DISTANCE_CUTOFF_PIX, OUTFILE):
     """
     If images have been classified and the text files are available, use this.
-    Clusters and collates based of yolov5 text files.
+    Clusters and collates from yolov5 text files.
     """
     classifications, _ = read_classifications("prerun", class_folder=dir)
     # cluster
@@ -133,46 +107,17 @@ def classify_text(dir):
     # process
     boats = process_clusters(clusters)
     # write to csv
-    write_to_csv(boats, "unknown")
+    write_to_csv(boats, "unknown", OUTFILE)
 
-def validate_input():
-    """
-    Validate that all input is correct.
-    :return: True if all input is valid, False otherwise.
-    """
-    if IMG_DIRECTORY is None and TIF_DIRECTORY is None and TEXT_DIRECTORY is None:
-        print("Please provide either a directory of image segments (-i) or a directory of tif images (-d) or a directory of text files (-t)")
-        exit(1)
-    # yolo path should exist
-    if not YOLO_PATH or not os.path.isdir(YOLO_PATH):
-        print("YOLO_PATH is not a valid directory")
-        return False
-    # WEIGHTS_PATH should be a valid .pt file
-    if not WEIGHTS_PATH or not os.path.isfile(WEIGHTS_PATH):
-        print("WEIGHTS_PATH is not a valid .pt file")
-        return False
-    # CLASS_PATH should be a valid directory
-    if not CLASS_PATH:
-        print("CLASS_PATH is not a valid directory")
-        return False
-    return True
-
-def init():
-    """
-    Populate global variables and initialise directories.
-    """
-    if TIF_DIRECTORY is None:
-        print("write_to_csv: This function cannot be called from an external script")
-        exit(1)
-    # get date from filename for each image
-    for image in os.listdir(TIF_DIRECTORY):
-        # check actually valid image:
-        if image[-4:] not in [".tif", ".png", "tiff"]:
-            continue
-        if (date := ics.get_date_from_filename(image)) not in days:
-            days.append(date)
-
-def detect(file=None, dir=None):
+def detect(
+        python, 
+        weights,
+        yolo_dir,
+        temp_dir = None,
+        file_name = None,
+        tif_dir = None,
+        img_dir = None, 
+        ):
     """
     Run the classifier 
     :param file: Tiff file to run the classifier on.
@@ -181,39 +126,34 @@ def detect(file=None, dir=None):
     :return:    Tuple of classifications > 50%, and low confidence classifications
                 The classifications as x, y, confidence, class, width, height, filename
     """
-    if TIF_DIRECTORY is None:
-        print("write_to_csv: This function cannot be called from an external script")
-        exit(1)
-    if file is None and dir is None:
-        print("detect: Please provide either a file or a directory")
-        exit(1)
-
-    # case 1 - tiff image (file) 
-    source = DATA_PATH
-    if file:
-        remove(DATA_PATH)
-        if DATA_PATH: os.mkdir(DATA_PATH) 
+    if temp_dir is None:
+        temp_dir = "temp"
+    remove(temp_dir)
+    source = temp_dir
+    # case 1 - tiff image (file)
+    if file_name:
+        os.mkdir(temp_dir) 
         remove("tempPNG")
         os.mkdir("tempPNG")
-        ics.create_padded_png(TIF_DIRECTORY, "tempPNG", file)
-        png_path = path.join(os.getcwd(), "tempPNG", f"{file[0:-4]}.png")
-        ics.segment_image_for_classification(png_path, DATA_PATH, 416, 104)
-    # case 2 - directory of images (dir)
-    elif dir:
-        source = dir
-        file = "dir" # for labelling in csv
-    detect_path = path.join(YOLO_PATH or "", "detect.py")
-    weights_path = path.join(WEIGHTS_PATH or "")
+        ics.create_padded_png(tif_dir, "tempPNG", file_name)
+        png_path = path.join(os.getcwd(), "tempPNG", f"{file_name[0:-4]}.png")
+        ics.segment_image_for_classification(png_path, temp_dir, 416, 104)
+    # case 2 - directory of segmented images (dir)
+    elif img_dir:
+        source = img_dir
+        file_name = "dir"                   # for labelling in csv
+    detect_path = path.join(yolo_dir, "detect.py")
     # run the command
-    os.system(f"{PYTHON_PATH} {detect_path} --imgsz 416 --save-txt --save-conf --weights {weights_path} --source {source}")
-    return read_classifications(file)
+    os.system(f"{python} {detect_path} --imgsz 416 --save-txt --save-conf --weights {weights} --source {source}")
+    return read_classifications(file_name, yolo_dir=yolo_dir, confidence_threshold=config["CONF_THRESHOLD"])
 
-def read_classifications(file, class_folder=None, confidence_threshold=CONF_THRESHOLD):
+def read_classifications(file, yolo_dir=None, class_folder=None, confidence_threshold=0.5):
     if class_folder is None:
+        assert yolo_dir is not None, "Must provide yolo_dir if class_folder is not provided"
         # Classifications are stored in the CLASS_PATH directory in the latest exp folder
-        exps = [int(f.split("exp")[1]) if f != "exp" else 0 for f in os.listdir(CLASS_PATH) if "exp" in f]
+        exps = [int(f.split("exp")[1]) if f != "exp" else 0 for f in os.listdir(os.path.join(yolo_dir, "runs", "detect" )) if "exp" in f]
         latest_exp = max(exps) if max(exps) != 0 else ""
-        classification_path = path.join(CLASS_PATH or "", f"exp{latest_exp}", "labels")
+        classification_path = path.join(os.path.join(yolo_dir, "runs", "detect"), f"exp{latest_exp}", "labels")
     else:
         classification_path = path.join(class_folder)
     # Store classifications as: (x, y, confidence, class, width, height)
@@ -292,24 +232,47 @@ def condense(cluster):
     thisBoatMean[2] = maxVals[2]
     return np.append(thisBoatMean, " ".join(files))
 
-def write_to_csv(classifications, day):
-    if OUTFILE is None:
-        print("write_to_csv: This function cannot be called from an external script")
-        exit(1)
+def write_to_csv(classifications, day, file):
     # Write to output csv
     # Create output csv if it doesn't exist
     print(len(classifications), len(classifications[0]))
     print(classifications[0])
-    if not os.path.isfile(f"{OUTFILE}.csv"):
-        with open(f"{OUTFILE}.csv", "a+") as outFile:
+    if not os.path.isfile(f"{file}.csv"):
+        with open(f"{file}.csv", "a+") as outFile:
             outFile.writelines("date,class,images,latitude,longitude,confidence,w,h\n")
 
     # Write the data for that day to a csv
     lines = [f"{day},{boat[3]},{boat[6]},{boat[1]},{boat[0]},{boat[2]},{float(boat[4])*416},{float(boat[5])*416}\n" for boat in classifications]
-    with open(f"{OUTFILE}.csv", "a+") as outFile:
+    with open(f"{file}.csv", "a+") as outFile:
         outFile.writelines(lines)
 
 ## Helper functions ## 
+def get_config():
+    """
+    parses command line args, and gets config.yml. 
+    Command line args have precedence and can be used to override config.yml
+    """
+    config:dict = yaml.safe_load(open("config.yml"))
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--directory",    help="Directory containing .tif files to be classified", required=False)
+    parser.add_argument("-i", "--images",       help="Directory of segmented 416x416 images", required=False)
+    parser.add_argument("-t", "--text",         help="Directory of text files with classifications", required=False)
+    parser.add_argument("-o", "--output",       help="Output file name", required=False)
+    args = parser.parse_args()
+    # command line args override config.yml
+    if args.directory:
+        config["tif_dir"] = args.directory
+    if args.images:
+        config["img_path"] = args.images
+    if args.text:
+        config["text_dir"] = args.text
+    if args.output:
+        config["OUTFILE"] = args.output
+    else:
+        if config.get("OUTFILE") is None:
+            config["OUTFILE"] = config["OUTFILE"].replace("<DATETIME>", datetime.now().strftime("%Y%m%d_%H%M%S"))
+    # replace <DATETIME> with current datetime
+    return config
 
 def remove(path, del_folder=True):
     """
@@ -326,22 +289,22 @@ def remove(path, del_folder=True):
     if del_folder:
         os.rmdir(path)
 
-def pixel2latlong(classifications, file):
+def pixel2latlong(classifications, file, tif_dir):
     """
     Convert the given classifications from pixel coordinates to lat/long.
     :param classifications: The classifications to convert, must have x, y as first two columns.
     :param file: The file these classifications came from.
     """
-    if TIF_DIRECTORY is None:
-        print("write_to_csv: This function cannot be called from an external script")
-        exit(1)
-    leftPad, _, topPad, _ = ics.get_required_padding(os.path.join(os.getcwd(), TIF_DIRECTORY, file))
+    leftPad, _, topPad, _ = ics.get_required_padding(os.path.join(os.getcwd(), tif_dir, file))
     for c in classifications:
         x = float(c[0]) - leftPad
         y = float(c[1]) - topPad
-        xp, yp = ics.pixel2coord(x, y, os.path.join(os.getcwd(), TIF_DIRECTORY, file))
+        xp, yp = ics.pixel2coord(x, y, os.path.join(os.getcwd(), tif_dir, file))
         c[0], c[1] = ics.coord2latlong(xp, yp)
     return classifications
 
 if __name__ == "__main__":
+    global config 
+    config = get_config()
     main()
+
