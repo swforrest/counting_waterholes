f3ee6102f3f81f492e2834f57c42cc7abafeecac
Charlie Turner <84701489+charlie-turner-314@users.noreply.github.com>
2024-04-22 08:33:31 +1000

    update where config gets file

--- counting_boats/utils/config.py
+++ counting_boats/utils/config.py
@@ -1,7 +1,9 @@
 import os
 import yaml
 
-with open('config.yml', 'r') as ymlfile:
+config_path = os.path.join(os.getcwd(), 'config.yml')
+
+with open(config_path, 'r') as ymlfile:
     cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
     cfg['tif_dir'] = os.path.join(cfg['proj_root'], 'images', 'RawImages') # This is generated so not included in the config file
 
