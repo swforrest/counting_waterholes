f96f8c72d96f4deb89b5907af5205af5eead5c1b
Scott Forrest <94874698+swforrest@users.noreply.github.com>
2025-03-27 18:36:43 +1000

    scripts working to download sentinel data and correctly create pngs

--- counting_boats/boat_utils/image_cutting_support.py
+++ counting_boats/boat_utils/image_cutting_support.py
@@ -688,10 +688,12 @@ def create_padded_png_S2(
     # Set the options for the gdal.Translate() call
     opsString2 = (
         # for planet data
-        # "-ot UInt16 -of png -b 3 -b 2 -b 1 -scale_1 0 2048 0 65535 -scale_2 0 2048 0 65535 -scale_3 0 2048 0 "
-        # "65535"
+        # "-ot UInt16 -of png -b 3 -b 2 -b 1 -scale_1 0 2048 0 65535 -scale_2 0 2048 0 65535 -scale_3 0 2048 0 65535"
         # for sentinel-2 data
-        "-ot UInt16 -of png -b 3 -b 2 -b 1 -scale_1 0 1 0 65535 -scale_2 0 1 0 65535 -scale_3 0 1 0 65535"
+        # for 0 to 255 scaling
+        # "-ot UInt16 -of png -b 3 -b 2 -b 1 -scale_1 0 255 0 65535 -scale_2 0 255 0 65535 -scale_3 0 255 0 65535"
+        # for 80 to 255 scaling (to give higher contrast based on min and max values of the sentinel-2 bands)
+        "-ot UInt16 -of png -b 1 -b 2 -b 3 -scale_1 80 255 0 65535 -scale_2 80 255 0 65535 -scale_3 80 255 0 65535"
     )
     # Translate original from tif into png
     print("Doing gdal work...")
